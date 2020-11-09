0e1d90f8b5efd2a7039d6ed5db459a27f16e620d
saraeggan <sara.eggan.d@gmail.com>
2020-11-09 11:23:13 +0100

    body-parser

--- server.js
+++ server.js
@@ -3,7 +3,10 @@
 
 const express = require('express');
 const path = require('path'); 
+const bodyParser = require('body-parser');
 const app = express(); 
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
 res.sendFile(path.join(__dirname, 'public', 'createUser.html'));
