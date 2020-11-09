a297a7477738a9630b1d193dfb9f68b1e2f61b7f
saraeggan <sara.eggan.d@gmail.com>
2020-11-09 11:04:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,11 @@
 
 const express = require('express');
 const path = require('path'); 
+const bodyParser = require('body-parser'); 
 const app = express(); 
 
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, 'public', 'index.html'));
+res.sendFile(path.join(__dirname, 'public', 'createUser.html'));
 
 }); 
 
