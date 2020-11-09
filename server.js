d12450bf02f239e2ad367b68eab1d658a113522f
EmilyThu <72381918+EmilyThu@users.noreply.github.com>
2020-11-09 11:26:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const express = require('express');
 const path = require('path');  
 const app = express(); 
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
 res.sendFile(path.join(__dirname, 'public', 'index.html'));
 
 }); 
+*/
+app.use(express.static('public'));
 
 app.post('/user',function (req, res) {
     
